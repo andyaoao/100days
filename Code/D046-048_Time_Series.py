4fe4887b583856240362ec3c944c87989935fc7b
andyaoao <andyaoao@gmail.com>
2018-09-26 01:01:03 +0900

    day 49 commit

--- Code/D046-048_Time_Series.py
+++ Code/D046-048_Time_Series.py
@@ -123,3 +123,58 @@ plt.plot()
 # 調整後もう一回テストした結果
 test_stationarity(new_ts)
 # テストの結果、p-value < 0.01 , 定常過程
+
+# 定常過程を確定になったら、定常過程用のモデルが使えるようになる
+# ARモデルを実装
+
+# 絵を描くfunction
+def tsplot(y, lags=None, figsize=(10, 8), style='bmh',title=''):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):
+        fig = plt.figure(figsize=figsize)
+        #mpl.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+
+        y.plot(ax=ts_ax)
+        ts_ax.set_title(title)
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return
+
+
+# AR(1)のモデルを実装
+np.random.seed(1)
+n_samples = int(1000)
+a = 0.6
+x = w = np.random.normal(size=n_samples)
+
+for t in range(n_samples):
+    x[t] = a*x[t-1] + w[t]
+limit=12
+_ = tsplot(x, lags=limit,title="AR(1)process")
+
+# AR(2)のモデルを実装
+n = int(1000)
+alphas = np.array([.444, .333])
+betas = np.array([0.])
+
+# Python requires us to specify the zero-lag value which is 1
+# Also note that the alphas for the AR model must be negated
+# We also set the betas for the MA equal to 0 for an AR(p) model
+# For more information see the examples at statsmodels.org
+ar = np.r_[1, -alphas]
+ma = np.r_[1, betas]
+
+ar2 = smt.arma_generate_sample(ar=ar, ma=ma, nsample=n)
+_ = tsplot(ar2, lags=12,title="AR(2) process")
